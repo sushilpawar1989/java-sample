3415d222505fe4fc6e67badc633941dfc610c841
Sushil Pawar <sushil.pawar@mynd.co>
2024-01-16 15:10:17 +0530

    test

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,6 +1,9 @@
 package hello;
 
 public class HelloWorld {
+
+	private int k =1;
+
 	public static void main(String[] args) {
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
